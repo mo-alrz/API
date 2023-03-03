7d3e349985105a78d6ca8c1831e94b3b6caf7f0a
Mohammad <m.alirezaaee.1986@gmail.com>
2023-03-03 20:43:10 +0100

    API exercises

--- Star Wars.py
+++ Star Wars.py
@@ -1,15 +1,40 @@
 import requests
 
-name = str(input('Please enter the name of character : '))
-response = requests.get(f'https://swapi.dev/api/people/?search={name}')
-jsonified = response.json()
+people = []
 
-data = ["name", "height", "mass", "hair_color", "skin_color", "eye_color"]
 
-for i in jsonified['results']:
-    for k,v in i.items():
-        if 'Skywalker' in v:
-            for x,z in i.items():
-                if x in data:
-                    print(f'{x} : {i[x]}')
-            print(f'-------------------')
+def all_people(api_path):
+    response = requests.get(api_path)
+    jsonified = response.json()
+    for i in jsonified['results']:
+        people.append(i['name'].lower())
+
+
+def find_char(user_input):
+    response = requests.get(f'https://swapi.dev/api/people/?search={user_input}')
+    jsonified = response.json()
+
+    data = ["name", "height", "mass", "hair_color", "skin_color", "eye_color"]
+
+    for c, i in enumerate(jsonified['results'], 1):
+        print(f'{c}---------------------')
+        for j in data:
+            print(f'{j.replace("_", " ").capitalize()} : {i[j]}')
+
+
+if __name__ == "__main__":
+
+    char = str(input('Please enter the name of character : ').lower())
+
+    all_people('https://swapi.dev/api/people/')
+
+    counter = len(people)
+    for i in people:
+        if char in i:
+            find_char(char)
+            break
+        if char not in i:
+            counter -= 1
+
+    if counter == 0:
+        print('The entered character is not in the list')
