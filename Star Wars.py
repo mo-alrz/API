47efcf5f30826511d29959b5d0142629d4319ed4
Mohammad <m.alirezaaee.1986@gmail.com>
2023-03-03 18:53:07 +0100

    API exercises

--- Star Wars.py
+++ Star Wars.py
@@ -1,11 +1,15 @@
 import requests
 
-name = "han solo"
+name = str(input('Please enter the name of character : '))
 response = requests.get(f'https://swapi.dev/api/people/?search={name}')
 jsonified = response.json()
 
-
 data = ["name", "height", "mass", "hair_color", "skin_color", "eye_color"]
 
-for i in data:
-    print(f'{i} : {jsonified["results"][0][i]}')
+for i in jsonified['results']:
+    for k,v in i.items():
+        if 'Skywalker' in v:
+            for x,z in i.items():
+                if x in data:
+                    print(f'{x} : {i[x]}')
+            print(f'-------------------')
